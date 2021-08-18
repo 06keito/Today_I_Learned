7a3e1ef1008d486b7cb8727c58b4c402a9fe503c
06keito <42394343+06keito@users.noreply.github.com>
2021-08-18 10:36:12 +0900

    add deepl translator function

--- share/Multiple_sentence_reconstruction/Multiple_sentence_reconstruction.py
+++ share/Multiple_sentence_reconstruction/Multiple_sentence_reconstruction.py
@@ -2,19 +2,31 @@
 # -*- coding: utf-8 -*-
 
 # Make each line consisting of multiple sentences a new line.
-# > pip install googletrans==4.0.0-rc1
-# > python Multiple_sentence_reconstruction.py
+# > pip install googletrans==4.0.0-rc1 deepl
+# > python Multiple_sentence_reconstruction.py --mode [google_trance,deepl]
 
-from googletrans import Translator
 import sys,re
+import argparse
 import regex
+import googletrans,deepl  
+#DeepL api Document -> #https://www.deepl.com/ja/blog/announcing-python-client-library-for-deepl-api
+
+CONSOLE_ARGUMENTS = None
+
+def get_args():
+     parser = argparse.ArgumentParser()
+     parser.add_argument('-m', '--mode',\
+          dest='mode', type=str, default='google_translate', help="choose mode")
+     return parser.parse_args()
+
+
 
 def input_text_data():
      S,data = str(),list()
      with open('input.txt',mode='r',encoding='utf-8') as fr:
           array = list(regex.split(r'(?<=[.])(?!$)',fr.read().replace('\n',' ')))
-     line_count = len(array) # get line len
-
+          # Separate each sentence.
+     line_count = len(array) # Get the number of lines
      if array[-1]!=' ': # add dummy line
           array += [' ']
           line_count += 1
@@ -26,14 +38,17 @@ def input_text_data():
           else:
                data.append(S)
                S = ''
+     return data
 
-     return data,line_count
+
 
 def print_error_log(error_frame_point,error_msg):
      print('{frame} : {msg}'\
           .format(frame=error_frame_point,msg=error_msg))
 
-def output_justification_text_data(data,line_count):
+
+
+def output_justification_text_data(data):
      with open('output.txt',mode='w',encoding='utf-8') as fw:
           for line in data:
                try:
@@ -42,21 +57,56 @@ def output_justification_text_data(data,line_count):
                     print_error_log(sys._getframe().f_code.co_name,error_msg)
                     continue
 
-def output_trancerated_text_data(data,line_count):
-     translator = Translator()
+
+
+def trancerate_with_the_selected_translators():
+     mode = CONSOLE_ARGUMENTS.mode
+     if mode=='deepl':
+          with open('deepl_auth_key.txt',mode='r',encoding='utf-8') as f:
+               auth_key = f.read() # get user auth key from deepl_auth_key.txt
+          try:
+               translator = deepl.Translator(auth_key)
+          except Exception as error_msg:
+               print_error_log(sys._getframe().f_code.co_name,error_msg)
+               translator = googletrans.Translator()
+     else:
+          translator = googletrans.Translator()
+
+     translator_class_name = str(translator.__class__)[8:-2]
+     # get sliced class name
+     # <class 'googletrans.client.Translator'>   ->   googletrans.client.Translator
+
+     print('> Using {}'.format(translator.__class__)) # show witch translator
+     return translator,translator_class_name
+
+
+
+def output_trancerated_text_data(data):
+     translator,translator_name = trancerate_with_the_selected_translators()
+
      with open('output_trancerated.txt',mode='w',encoding='utf-8') as fw:
-          for line in data:
-               try:
-                    tr = translator.translate(line,src='en', dest='ja').text
-                    fw.write('{l_br}'.format(l_br=tr+'\n'))
-               except Exception as error_msg:
-                    print_error_log(sys._getframe().f_code.co_name,error_msg)
-                    continue
+          try:
+               if translator_name == 'googletrans.client.Translator':
+                    for line in data:
+                         trancerated_text = translator.translate(line,src='en', dest='ja').text
+                         fw.write('{l_br}'.format(l_br=trancerated_text+'\n'))
+               elif translator_name == 'deepl.translator.Translator':
+                    for line in data:
+                         trancerated_text = translator.translate_text(line, target_lang='JA').text
+                         # If you have selected the Free Plan, there may be a character limit.
+                         fw.write('{l_br}'.format(l_br=trancerated_text+'\n'))
+
+          except Exception as error_msg:
+               print_error_log(sys._getframe().f_code.co_name,error_msg)
+
+
 
 def main():
-     text_data,line_count = input_text_data() #get text data
-     output_justification_text_data(text_data,line_count)
-     output_trancerated_text_data(text_data,line_count)
+     global CONSOLE_ARGUMENTS
+     CONSOLE_ARGUMENTS = get_args()
+     text_data = input_text_data() #get text data
+     output_justification_text_data(text_data)
+     output_trancerated_text_data(text_data)
 
 if __name__ == '__main__':
      main()
