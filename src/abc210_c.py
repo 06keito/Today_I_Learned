30dc738e9c3fe7926e740450c33f1974dcb01cc4
06keito <keito.yamamoto06@gmail.com>
2021-09-15 01:27:47 +0900

    slove task

--- src/abc210_c.py
+++ src/abc210_c.py
@@ -1,28 +1,31 @@
-import collections
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def bool_check():
-    global flag
-    if flag == "True":
-        flag = "False"
-        return True
-    else:
-        False
+# Reference : https://blog.hamayanhamayan.com/entry/2021/07/17/233052
+
+from collections import Counter
 
 def main():
     N,K = map(int,input().split())
     C = list(map(int,input().split()))
-    dict = collections.defaultdict(int)
-    ans = 0
-
-    for i,a in enumerate(C):
-        dict[a] += 1
-        if i>=K:
-            dict[C[i-K]]-=1
-            if dict[C[i-K]]==0:
-                dict.pop(C[i-K])
-        if i>=K-1:
-            ans = max(ans,len(dict.keys()))
+    cnt = Counter()
+    kind = ans = 0
+    # すべての種類のキャンディとその個数を数え上げる
+    for i,c in enumerate(C):
+        cnt[c] += 1
+        # 新しい種類を範囲内に見つけたときの処理
+        if cnt[c] == 1:
+            kind += 1
+        # Kの範囲を超えた場合の処理
+        if i >= K:
+            # 種類を記録した配列を操作する
+            # i-Kの配列の場所は範囲から外れるため、数え上げから除外する
+            cnt[C[i-K]] -= 1
+            # もしその種類の飴がなくなってしまった場合
+            if cnt[C[i-K]] == 0:
+                kind -= 1
+        ans = max(kind, ans)
     print(ans)
-if __name__ == "__main__":
-    flag = "True"
+
+if __name__ == '__main__':
     main()
